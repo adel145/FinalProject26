915f6b88684bbc2688119a372351dd3a4adb5a13
adel145 <adelmohsen145@gmail.com>
2026-01-15 14:38:28 +0200

    pages udpates by Adel

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
+       base: '/FinalProject26/',   // 👈 זה שם הריפו שלך
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
